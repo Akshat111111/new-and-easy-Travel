0e6314bbaab17ee3a0e05faa5c42c6edb637879e
Aditya Singh <adityasinghdasila15@gmail.com>
2024-05-30 10:22:48 +0530

    login and DB module complete

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ Db.connect();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+
+
+
+
+//GET
 app.get("/",(req,res)=>{
     try{
         res.render("intro.ejs");
@@ -34,12 +39,19 @@ app.get("/register",(req,res)=>{
     res.render("register.ejs",{});
 });
 
-app.get("/login",(req,res)=>{
-    console.log("login route has been hit");
-    
+app.get("/login",async (req,res)=>{
+    try{
+        res.render("login.ejs",{});
+    }
+    catch(error){
+        console.log(error.message);
+    }
 });
 
 
+
+
+//POST
 app.post("/register",async(req,res)=>{
     try{
         console.log("register post hit");
@@ -63,5 +75,27 @@ app.post("/register",async(req,res)=>{
     }
 });
 
+app.post("/login",async (req,res)=>
+{
+    console.log("login route has been hit");
+    try{
+        let email=req.body.email;
+        let pswd= req.body.password;
+        let result=await Db.query("select * from users where email = $1",[email]);
+        console.log(result.rows);
+        if(result.rows.length>0){
+            let realpswd = result.rows[0].password;
+            if(realpswd==pswd){
+                res.render("index.ejs",{username:result.rows[0].name});
+            }
+            else{
+                res.send("<h6>email and password do not match</h6>");
+            }
+        }
+    }
+    catch(error){
+        console.log(error.message);
+    }
+});
 
 app.listen(port,()=>console.log("The server has started at port number: "+port));
